089572438fd17b24cc8a9111eb127043ce19e321
xis <xishang7707@163.com>
2026-01-04 09:55:08 +0800

    调整ATR计算

--- src/indicators/atr.rs
+++ src/indicators/atr.rs
@@ -1,15 +1,14 @@
-use serde::{Deserialize, Serialize};
 use crate::core::{
 	Error, IndicatorConfig, IndicatorInstance, IndicatorResult, Method, MovingAverageConstructor,
 	PeriodType, ValueType, OHLCV,
 };
 use crate::helpers::MA;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Copy)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct ATR<M: MovingAverageConstructor = MA> {
-	pub method1: M,
-	pub method2: M,
+	pub ma: M,
 	pub period: PeriodType,
 }
 
@@ -18,18 +17,14 @@ impl<M: MovingAverageConstructor> IndicatorConfig for ATR<M> {
 	const NAME: &'static str = "ATR";
 
 	fn validate(&self) -> bool {
-		self.method1.ma_period() >= 1 && self.method2.ma_period() >= 1 && self.period >= 1
+		self.ma.ma_period() >= 1
 	}
 
 	fn set(&mut self, name: &str, value: String) -> Result<(), Error> {
 		match name {
-			"method1" => match value.parse() {
+			"ma" => match value.parse() {
 				Err(_) => return Err(Error::ParameterParse(name.to_string(), value.to_string())),
-				Ok(value) => self.method1 = value,
-			},
-			"method2" => match value.parse() {
-				Err(_) => return Err(Error::ParameterParse(name.to_string(), value.to_string())),
-				Ok(value) => self.method2 = value,
+				Ok(value) => self.ma = value,
 			},
 			"period" => match value.parse() {
 				Err(_) => return Err(Error::ParameterParse(name.to_string(), value.to_string())),
@@ -53,12 +48,11 @@ impl<M: MovingAverageConstructor> IndicatorConfig for ATR<M> {
 		}
 
 		let cfg = self;
-		let tr = candle.tr(candle);
+		let tr = candle.tr_close(f64::NAN);
 
 		Ok(Self::Instance {
 			prev_close: candle.close(),
-			tr_ma: cfg.method1.init(tr)?,
-			ma: cfg.method2.init(0.0)?,
+			ma: cfg.ma.init(tr)?,
 			cfg,
 		})
 	}
@@ -67,8 +61,7 @@ impl<M: MovingAverageConstructor> IndicatorConfig for ATR<M> {
 impl Default for ATR {
 	fn default() -> Self {
 		Self {
-			method1: MA::RMA(14),
-			method2: MA::RMA(14),
+			ma: MA::RMA(14),
 			period: 14,
 		}
 	}
@@ -80,7 +73,6 @@ pub struct ATRInstance<M: MovingAverageConstructor = MA> {
 	cfg: ATR<M>,
 
 	prev_close: ValueType,
-	tr_ma: M::Instance,
 	ma: M::Instance,
 }
 
@@ -92,9 +84,7 @@ impl<M: MovingAverageConstructor> IndicatorInstance for ATRInstance<M> {
 	}
 
 	fn next<T: OHLCV>(&mut self, candle: &T) -> IndicatorResult {
-		let atr = self
-			.ma
-			.next(&candle.tr_close(self.prev_close));
+		let atr = self.ma.next(&candle.tr_close(self.prev_close));
 		let values = [atr];
 		IndicatorResult::new(&values, &[])
 	}
